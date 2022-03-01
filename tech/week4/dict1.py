bfa5ce5d0c5f2fb65ee7c9e4fee9a780f50cfff3
sakshamdeveloper5821 <99405530+sakshamdeveloper5821@users.noreply.github.com>
2022-03-01 18:18:11 +0530

    Add files via upload

---
+++ tech/week4/dict1.py
@@ -0,0 +1,5 @@
+wardrobe = {"shirt":["red","blue","white"], "jeans":["blue","black"]}
+for i in wardrobe.keys():
+    k=wardrobe[i]
+    for j in k:
+        print(j,i)
---
+++ tech/week4/strip.py
@@ -0,0 +1,7 @@
+a="    Never Odd or Even  ".replace(' ','')
+c=a.upper()
+b=c[::-1]
+if c==b:
+	print('plindrome')
+else:
+	print('not')	
